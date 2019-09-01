223aca3523e6b2f052cb4b325355c0b8b2c18e28
Minkyu Jeon <ayaa66@naver.com>
2019-09-01 10:36:42 +0900

    task_action_log type에 resolution method 추가

--- app/graphql/types/task_action_log.rb
+++ app/graphql/types/task_action_log.rb
@@ -3,7 +3,7 @@ module Types
     description "태스크 액션 로그 타입"
 
     field :task, Types::TaskType, null: false
-    field :before_workflow, Types::WorkflowType, null: true
-    field :current_workflow, Types::WorkflowType, null: false
+    field :before_workflow, Types::WorkflowType, resolve: -> (task_action_log, _args, _ctx) { task_action_log.before_workflow }, null: true
+    field :current_workflow, Types::WorkflowType, resolve: -> (task_action_log, _args, _ctx) { task_action_log.current_workflow }, null: false
   end
 end
