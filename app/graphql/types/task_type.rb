583c78907fa2a78215987a21db584618e9d42991
Minkyu Jeon <ayaa66@naver.com>
2019-09-01 10:35:58 +0900

    task type에 resolution method 추가

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -6,11 +6,11 @@ module Types
     field :description, GraphQL::STRING_TYPE, null: true
     field :priority, GraphQL::INT_TYPE, null: false
     field :task_type, GraphQL::INT_TYPE, null: false
-    field :project, Types::ProjectType, null: false
-    field :board, Types::BoardType, null: false
-    field :assignee, Types::UserType, null: true
-    field :registrant, Types::UserType, null: false
-    field :workflow, Types::WorkflowType, null: false
-    field :comments, [Types::CommentType], null: true
+    field :project, Types::ProjectType, resolve: -> (task, _args, _ctx) { task.project }, null: false
+    field :board, Types::BoardType, resolve: -> (task, _args, _ctx) { task.board }, null: false
+    field :assignee, Types::UserType, resolve: -> (task, _args, _ctx) { task.assignee }, null: true
+    field :registrant, Types::UserType, resolve: -> (task, _args, _ctx) { task.registrant }, null: false
+    field :workflow, Types::WorkflowType, resolve: -> (task, _args, _ctx) { task.workflow }, null: false
+    field :comments, [Types::CommentType], resolve: -> (task, _args, _ctx) { task.comments }, null: true
   end
 end
