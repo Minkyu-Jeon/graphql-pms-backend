76035b47b0695721dbfda536411161528b388991
Minkyu Jeon <ayaa66@naver.com>
2019-09-01 10:33:44 +0900

    board type에 resolution method 추가

--- app/graphql/types/board_type.rb
+++ app/graphql/types/board_type.rb
@@ -3,7 +3,7 @@ module Types
     description "보드 모델"
 
     field :board_type, GraphQL::STRING_TYPE, null: false
-    field :project, Types::ProjectType, null: false
-    field :workflows, [Types::WorkflowType], null: false
+    field :project, Types::ProjectType, resolve: -> (board, _args, _ctx) { board.project }, null: false
+    field :workflows, [Types::WorkflowType], resolve: -> (board, _args, _ctx) { board.workflows }, null: false
   end
 end
