0f2238679739bf8ecf6de55d7b6ecd8642ef9e23
Minkyu Jeon <ayaa66@naver.com>
2019-09-02 21:09:14 +0900

    signin mutation 등록

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::Users::Create
     field :create_project, mutation: Mutations::Projects::Create
+    field :signin, mutation: Mutations::Sessions::Create
   end
 end
