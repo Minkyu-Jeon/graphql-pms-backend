29fbaa2a1872d92b7344d727e5f3eb49800129d3
Minkyu Jeon <ayaa66@naver.com>
2019-09-02 21:07:27 +0900

    project create mutation 추가

--- app/graphql/mutations/projects/create.rb
+++ app/graphql/mutations/projects/create.rb
@@ -1,7 +1,31 @@
 module Mutations
   module Projects
     class Create < Mutations::BaseMutation
+      argument :name, String, required: false
+      argument :description, String, required: true
 
+      field :project, Types::ProjectType, null: true
+      field :errors, [String], null: false
+
+      def resolve(name:, description:, board_type:)
+        project = Project.new(name: name, description: description)
+
+        project.boards.build(board_type: board_type)
+
+        project.users << context[:current_user]
+        
+        if project.save
+          {
+            project: project,
+            errors: []
+          }
+        else
+          {
+            project: null,
+            errors: project.errors.full_messages
+          }
+        end
+      end
     end
   end
 end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::Users::Create
+    field :create_project, mutation: Mutations::Projects::Create
   end
 end
